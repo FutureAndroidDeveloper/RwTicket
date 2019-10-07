7aeed0b5a122abb362426a8eb19010fbe3259e89
Кирилл Клименков <dev.kirill.klimenkov@gmail.com>
2019-10-07 15:36:18 -0700

    Init new project

---
+++ RwTickets/Pods/Target Support Files/Pods-RwTickets/Pods-RwTickets-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RwTickets : NSObject
+@end
+@implementation PodsDummy_Pods_RwTickets
+@end
---
+++ RwTickets/RwTickets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RwTickets
+//
+//  Created by Kiryl Klimiankou on 10/7/19.
+//  Copyright © 2019 Kiryl Klimiankou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
